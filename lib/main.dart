974dcd105490562ecfc864db54ba144d3b721ba3
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-02-25 12:56:32 +0500

    beautiful food app design

---
+++ lib/main.dart
@@ -0,0 +1,422 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Dashboard());
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Color(0xff12151e),
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff12151e),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 70,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hello,\nKristen",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/15545223/pexels-photo-15545223.jpeg"),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: createRecommend(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15, left: 8.0, right: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Popular 3253",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  ImageIcon(
+                    NetworkImage(
+                        "https://cdn-icons-png.flaticon.com/512/9702/9702724.png"),
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: ListView(
+                  children: recommendList(),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+
+  List<Widget> createRecommend() {
+    List<Widget> pills = [];
+
+    List<String> food = [
+      'Popular',
+      'Breakfast',
+      'Salad',
+      'Burger',
+      'Roast',
+      'Fries',
+      'Fish'
+    ];
+    List<String> foodIconLink = [
+      'https://cdn-icons-png.flaticon.com/512/599/599502.png',
+      'https://cdn-icons-png.flaticon.com/512/7093/7093198.png',
+      'https://cdn-icons-png.flaticon.com/512/3075/3075977.png',
+      'https://cdn-icons-png.flaticon.com/512/135/135715.png',
+      'https://cdn-icons-png.flaticon.com/512/1046/1046751.png',
+      'https://cdn-icons-png.flaticon.com/512/590/590717.png',
+      'https://cdn-icons-png.flaticon.com/512/8737/8737671.png'
+    ];
+    int i = 0;
+    while (i < food.length) {
+      Padding pill = Padding(
+        padding: EdgeInsets.all(5),
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white54),
+              borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 20,
+                  child: Image(image: NetworkImage(foodIconLink[i])),
+                ),
+                Text(
+                  food[i],
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+      pills.add(pill);
+      i++;
+    }
+    return pills;
+  }
+
+  List<Widget> createFoodCard(BuildContext context) {
+    List<Widget> foodCards = [];
+    List<String> foodImages = [
+      './images/1.jpg',
+      './images/2.jpg',
+      './images/3.jpg',
+      './images/4.jpg',
+      './images/5.jpg',
+    ];
+
+    int i = 0;
+
+    while (i < 5) {
+      Padding foodcard = Padding(
+        padding: EdgeInsets.all(10),
+        child: Material(
+          color: Color(0xff12151e),
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => openFood(foodImages[i - 1]),
+                  ));
+            },
+            child: Ink(
+              decoration: BoxDecoration(
+                  color: Color(0xff171f2c),
+                  borderRadius: BorderRadius.circular(20)),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                          radius: 45,
+                          backgroundImage: AssetImage(foodImages[i])),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "60kcl",
+                              style: TextStyle(
+                                  color: Color(0xffd1f64f), fontSize: 12),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text("789",
+                                style: TextStyle(
+                                    color: Color(0xffd1f64f), fontSize: 12))
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5, bottom: 5),
+                          child: Text("Slicing with Fruit",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        Text(
+                            "This is good food to eat\n Food with lots of benifits\n Try this out now",
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 199, 199, 199)))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+      foodCards.add(foodcard);
+      i++;
+    }
+    return foodCards;
+  }
+
+  Widget openFood(String imagelink) {
+    BuildContext context;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff12151e),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: ImageIcon(NetworkImage(
+                  "https://cdn-icons-png.flaticon.com/512/2961/2961957.png")))
+        ],
+        title: Center(child: Text("Fresh Rice")),
+      ),
+      backgroundColor: Color(0xff12151e),
+      body: Column(
+        children: [
+          CircleAvatar(
+            radius: 150,
+            backgroundImage: AssetImage(imagelink),
+          ),
+          // Container(
+          //   color: Colors.red,
+          //   child: Image.asset(),
+          // ),
+          Container(
+            margin: EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(color: Color.fromARGB(255, 187, 187, 187))),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "799",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                      Text(
+                        "Kcal",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "799",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Kcal",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "799",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Kcal",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "799",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Kcal",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "799",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Kcal",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 52, 59, 80),
+                borderRadius:
+                    BorderRadius.vertical(top: Radius.elliptical(50, 50))),
+            child: ListView(
+              children: recommendList(),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+
+  List<Widget> recommendList() {
+    List<Widget> foodList = [];
+    List<String> foodImages = [
+      './images/1.jpg',
+      './images/2.jpg',
+      './images/3.jpg',
+      './images/4.jpg',
+      './images/5.jpg',
+    ];
+
+    int i = 0;
+
+    while (i < 5) {
+      Padding foodcard = Padding(
+        padding:
+            const EdgeInsets.only(left: 15.0, right: 15, top: 2, bottom: 2),
+        child: Container(
+          margin: EdgeInsets.only(top: 20),
+          decoration: BoxDecoration(
+              color: Color(0xff171f2c),
+              borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                      radius: 45, backgroundImage: AssetImage(foodImages[i])),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "60kcl",
+                          style:
+                              TextStyle(color: Color(0xffd1f64f), fontSize: 12),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text("789",
+                            style: TextStyle(
+                                color: Color(0xffd1f64f), fontSize: 12))
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Text("Slicing with Fruit",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    Text(
+                        "This is good food to eat\n Food with lots of benifits\n Try this out now",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Color.fromARGB(255, 199, 199, 199)))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+      foodList.add(foodcard);
+      i++;
+    }
+    return foodList;
+  }
+}
