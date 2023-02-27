a5409f86e0c063f2a43f8dfcdd6476409c5973d6
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-02-27 22:29:24 +0500

    prodcut screen ready

--- lib/Beauty Products/product_screen.dart
+++ lib/Beauty Products/product_screen.dart
@@ -6,13 +6,85 @@ class SkinProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        backgroundColor: Colors.white,
         body: Column(children: [
-          Row(
-            children: [Text("Data"), Text("Data")],
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+                Text(
+                  "Normal Skin",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                          color: Color.fromARGB(255, 202, 202, 202))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.shopping_cart,
+                      color: Color.fromARGB(255, 252, 225, 240),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+            child: TextField(
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                        onPressed: () {}, icon: Icon(Icons.search_outlined)),
+                    hintText: "Search products for skin",
+                    filled: true,
+                    fillColor: Color(0xfff8f8f8),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none))),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "10 Products Result",
+                  style: TextStyle(
+                      fontSize: 12, color: Color.fromARGB(255, 173, 173, 173)),
+                ),
+                Container(
+                    height: 40,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Color.fromARGB(255, 228, 228, 228)),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "Sort by",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Icon(
+                            Icons.arrow_downward,
+                            size: 15,
+                          )
+                        ]))
+              ],
+            ),
           ),
           Expanded(
             child: GridView.builder(
+              physics: ScrollPhysics(),
               itemCount: ProductModel.productsSender().length,
               gridDelegate:
                   SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
@@ -29,13 +101,20 @@ class SkinProductScreen extends StatelessWidget {
         width: 100,
         height: 150,
         decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromARGB(232, 214, 214, 214),
+                  spreadRadius: 0.5,
+                  offset: Offset(1, 1),
+                  blurRadius: 1)
+            ],
             color: Color.fromARGB(255, 252, 225, 240),
             borderRadius: BorderRadius.circular(10)),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             Container(
-              margin: EdgeInsets.only(top: 20),
+              margin: EdgeInsets.only(top: 10),
               height: 90,
               decoration: BoxDecoration(
                   image: DecorationImage(
