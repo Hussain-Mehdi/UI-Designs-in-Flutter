276b4b4369829da93ed00a863d7fa392794b3806
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-03-04 23:21:02 +0500

    gestuer detector

--- lib/shoes_App/shoes_dashboard.dart
+++ lib/shoes_App/shoes_dashboard.dart
@@ -97,17 +97,14 @@ class _ShoesDashboardState extends State<ShoesDashboard> {
                 )),
           ),
           Expanded(
-            child: ListView(
-              children: createShoesCards(),
-            ),
+            child: createShoesCards(),
           ),
         ],
       ),
     );
   }
 
-  List<Widget> createShoesCards() {
-    List<Widget> shoesCards = [];
+  Widget createShoesCards() {
     List<String> shoesDetail = [
       'Nike',
       'Kyrie',
@@ -118,11 +115,9 @@ class _ShoesDashboardState extends State<ShoesDashboard> {
       './images/s2.png',
       './images/s3.png',
     ];
-    int i = 0;
-    while (i < 4) {
-      Padding card = Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        child: GestureDetector(
+
+    return ListView.builder(itemBuilder: (context, index) {
+      return GestureDetector(
           onTap: () {
             setState(() {
               if (clicked) {
@@ -132,112 +127,110 @@ class _ShoesDashboardState extends State<ShoesDashboard> {
               }
             });
           },
-          child: SizedBox(
-            width: 340,
-            height: 120,
-            child: Stack(children: [
-              Container(
-                height: 110,
+          child: Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: SizedBox(
                 width: 340,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color(0xff201d2e)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 285.0),
-                  child: IconButton(
-                    color: Color.fromARGB(255, 247, 247, 247),
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.shopping_cart_outlined,
+                height: 120,
+                child: Stack(children: [
+                  Container(
+                    height: 110,
+                    width: 340,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color(0xff201d2e)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 285.0),
+                      child: IconButton(
+                        color: Color.fromARGB(255, 247, 247, 247),
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.shopping_cart_outlined,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                width: 300,
-                height: 110,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color(0xff47435c),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 245.0),
-                  child: IconButton(
-                      color: Color.fromARGB(255, 247, 247, 247),
-                      onPressed: () {},
-                      icon: ImageIcon(AssetImage("./images/heart.png"))),
-                ),
-              ),
-              AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                height: 110,
-                width: clicked ? 250 : 340,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color.fromARGB(255, 255, 255, 255)),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AnimatedContainer(
-                      duration: const Duration(seconds: 1),
-                      margin: const EdgeInsets.only(left: 5),
-                      height: clicked ? 110 : 140,
-                      width: clicked ? 110 : 140,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(shoesDetail[i + 4]))),
+                  Container(
+                    width: 300,
+                    height: 110,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xff47435c),
                     ),
-                    const SizedBox(
-                      width: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 245.0),
+                      child: IconButton(
+                          color: Color.fromARGB(255, 247, 247, 247),
+                          onPressed: () {},
+                          icon: ImageIcon(AssetImage("./images/heart.png"))),
                     ),
-                    AnimatedContainer(
-                      duration: const Duration(seconds: 1),
-                      margin: EdgeInsets.only(
-                        left: clicked ? 10 : 20,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            shoesDetail[i],
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
-                                color: Color(0xff0a3e7b)),
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    height: 110,
+                    width: clicked ? 250 : 340,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(255, 255, 255, 255)),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          margin: const EdgeInsets.only(left: 5),
+                          height: clicked ? 110 : 140,
+                          width: clicked ? 110 : 140,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(shoesDetail[index + 4]))),
+                        ),
+                        const SizedBox(
+                          width: 3,
+                        ),
+                        AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          margin: EdgeInsets.only(
+                            left: clicked ? 10 : 20,
                           ),
-                          AnimatedContainer(
-                            duration: const Duration(seconds: 1),
-                            margin: const EdgeInsets.only(top: 5, bottom: 20),
-                            width: clicked ? 100 : 130,
-                            child: const Text(
-                              "Find your wings with off-road run",
-                              style: TextStyle(
-                                  fontStyle: FontStyle.italic,
-                                  fontSize: 10,
-                                  color: Color.fromARGB(151, 10, 63, 123)),
-                            ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                shoesDetail[index],
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff0a3e7b)),
+                              ),
+                              AnimatedContainer(
+                                duration: const Duration(seconds: 1),
+                                margin:
+                                    const EdgeInsets.only(top: 5, bottom: 20),
+                                width: clicked ? 100 : 130,
+                                child: const Text(
+                                  "Find your wings with off-road run",
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 10,
+                                      color: Color.fromARGB(151, 10, 63, 123)),
+                                ),
+                              ),
+                              const Text(
+                                "125.00",
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff0a3e7b)),
+                              ),
+                            ],
                           ),
-                          const Text(
-                            "125.00",
-                            style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                                color: Color(0xff0a3e7b)),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ]),
-          ),
-        ),
-      );
-      shoesCards.add(card);
-      i++;
-    }
-    return shoesCards;
+                        )
+                      ],
+                    ),
+                  ),
+                ]),
+              )));
+    });
   }
 }
