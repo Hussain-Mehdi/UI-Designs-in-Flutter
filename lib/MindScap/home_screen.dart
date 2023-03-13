fdfe0f94bd31f005b85f6c6c2690f281db2bab3f
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-03-13 23:59:39 +0500

    home screen

---
+++ lib/MindScap/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                "./images/mindscape/Dashboard.jpg",
+              ),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 100, right: 0, top: 70),
+              child: Text(
+                "MINDSCAPE",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 5,
+                    color: Colors.white),
+              ),
+            ),
+            Container(
+                height: 250,
+                margin: EdgeInsets.only(bottom: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 18.0, right: 18),
+                      child: Text(
+                        "Yes. We're always interested in improving this generator and one of the best ways to do that is to add new and interesting paragraphs to the generator. If you'd like to contribute some random paragraphs, please contact us.Yes. We're always interested in improving this generator and one of the best ways to do that is to add new and interesting paragraphs to the generator. If you'd like to contribute some random paragraphs, please contact us.",
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 18),
+                      width: 150,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Center(
+                        child: Text(
+                          "Explore",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
