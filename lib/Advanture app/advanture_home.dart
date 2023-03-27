970b647cfa5e74545b531aee47eed3b5856b659a
Hussain-Mehdi <75681588+Hussain-Mehdi@users.noreply.github.com>
2023-03-27 22:31:16 +0500

    home screen

--- lib/Advanture app/advanture_home.dart
+++ lib/Advanture app/advanture_home.dart
@@ -17,6 +17,7 @@ class AdventureHome extends StatelessWidget {
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             "Ready for your next\nadventure?",
@@ -25,16 +26,22 @@ class AdventureHome extends StatelessWidget {
                 fontSize: 25,
                 fontWeight: FontWeight.w900),
           ),
-          Row(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5))),
-              ),
-              Icon(Icons.mic)
-            ],
-          )
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 30),
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  prefixIcon: Icon(Icons.search),
+                  hintText: "Search destination",
+                  hintStyle:
+                      TextStyle(color: Color.fromARGB(255, 172, 172, 172)),
+                  filled: true,
+                  fillColor: Color(0xffedebee),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none)),
+            ),
+          ),
         ],
       ),
     );
