2290ad690aaf002fbb6a285dfd80a4fb1966c792
Hussain-Mehdi <75681588+Hussain-Mehdi@users.noreply.github.com>
2023-04-23 22:33:37 +0500

    fitness created

--- lib/fitness app/fitness_homescreen.dart
+++ lib/fitness app/fitness_homescreen.dart
@@ -13,30 +13,37 @@ class FitnessHomeScreen extends StatelessWidget {
         actions: [
           Icon(
             Icons.alarm_on_outlined,
-            color: Colors.black26,
+            color: Colors.black,
           ),
           SizedBox(
             width: 10,
           ),
           Icon(
             Icons.shopping_cart_rounded,
-            color: Colors.black26,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 10,
           ),
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Store",
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: 30,
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0, bottom: 15),
+            child: Text(
+              "Store",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 30,
+              ),
             ),
           ),
           Expanded(
               child: SizedBox(
             child: ListView(
-              children: [],
+              children: cardList(context),
             ),
           ))
         ],
@@ -44,13 +51,56 @@ class FitnessHomeScreen extends StatelessWidget {
     );
   }
 
-  List<Widget> cardList() {
+  List<Widget> cardList(BuildContext context) {
     int i = 0;
+    List<Widget> cards = [];
     List<String> cardData = [
       'Traning Plan',
       'Meal Plan',
       'Supplement',
-      'Fitness'
+      'Fitness',
+      'Body Building',
+      'images/fitness/fit1.jpg',
+      'images/fitness/fit2.jpg',
+      'images/fitness/fit3.jpg',
+      'images/fitness/fit4.jpg',
+      'images/fitness/fit5.jpg',
     ];
+
+    while (i < 5) {
+      Padding card = Padding(
+        padding: const EdgeInsets.only(left: 18.0, right: 18, bottom: 10),
+        child: Container(
+          // decoration:
+          //  BoxDecoration(borderRadius: BorderRadius.circular(15)),
+          height: 180,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              Container(
+                height: 180,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                        image: AssetImage(cardData[i + 4]), fit: BoxFit.cover)),
+              ),
+              Positioned(
+                bottom: 10,
+                left: 10,
+                child: Text(cardData[i],
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      );
+      cards.add(card);
+      i++;
+    }
+    return cards;
   }
 }
